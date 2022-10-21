b66e2f5784de2895649aef479f28149e0e32ffd8
Kieran Bissessar <bissessk@outlook.com>
2022-10-21 00:27:51 -0400

    [SWE-002] Add Flake8 | update yml, add .flake8, and remove redundant imports

--- ebdjangoTest9/polls/views.py
+++ ebdjangoTest9/polls/views.py
@@ -1,9 +1,6 @@
-from django.http import HttpResponse
-from django.http import Http404
-from django.template import loader
-from django.shortcuts import get_object_or_404, render
-
-from .models import Question
+# from django.http import HttpResponse
+# from django.http import Http404
+# from django.template import loader
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
